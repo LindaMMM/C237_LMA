d3ac187359c62bbb22d22a301f22b2b56913f07f
Linda M <linda_benchabane@hotmail.com>
2024-03-30 16:47:45 +0100

    .

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class FrontendController extends AbstractController
+class BackendController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/backend', name: 'app_backend')]
     public function index(): Response
     {
-        return $this->render('frontend/index.html.twig', [
-            'controller_name' => 'FrontendController',
+        return $this->render('backend/index.html.twig', [
+            'controller_name' => 'BackendController',
         ]);
     }
 }
